550d7c611f8269ff5b8acf48a19b4b9b1c126472
Hanra <37299738+HenraL@users.noreply.github.com>
2024-08-06 22:33:10 +0100

    [FIX] font changing for main page

--- frontend/assets/js/scripts/header/settings/fonts.js
+++ frontend/assets/js/scripts/header/settings/fonts.js
@@ -37,33 +37,44 @@ function get_fonts_if_not_fetched() {
     return JSON.parse(localStorage.getItem(available_fonts_key));
 }
 
-function change_font(font_name) {
+function change_font(font_name, theme) {
     console.log(`(change_font) font_name = "${font_name}"`);
     const root = document.documentElement,
-        font_targets = ['--global-family'];
+        css_theme = get_correct_prefix(theme),
+        empty_entry = `["--global-family","--global-table-list-family","--global-table-list-title-family","--global-header-family","--global-main-family","--global-footer-family"]`,
+        default_entry = `["--main-global-family","--main-header-family","--main-main-family","--main-footer-family","--main-table-list-family","--main-table-list-title-family"]`,
+        darling_entry = `[]`,
+        poems_entry = `[]`,
+        quotes_entry = `[]`,
+        font_targets = JSON.parse(`{ "${get_correct_prefix("")}": ${empty_entry}, "${get_correct_prefix("default")}": ${default_entry}, "${get_correct_prefix("darling")}": ${darling_entry}, "${get_correct_prefix("poems")}": ${poems_entry} , "${get_correct_prefix("quotes")}": ${quotes_entry}}`);
     document.body.style.fontFamily = font_name;
-    for (var i in font_targets) {
-        root.style.setProperty(font_targets[i], font_name);
+    if (css_theme in font_targets) {
+        console.log(`(change_font) ${theme} css_theme = ${css_theme} is present in ${JSON.stringify(font_targets)}`);
+        for (var i in font_targets[css_theme]) {
+            root.style.setProperty(font_targets[css_theme][i], font_name);
+        }
+    } else {
+        console.error(`(change_font) Theme not found: ${theme}, ${css_theme} in ${JSON.stringify(font_targets)}`);
     }
 
 }
 
-function _get_users_choice(evt) {
+function _get_users_choice(evt, theme) {
     const font = evt.target.value;
-    change_font(font);
+    change_font(font, theme);
 }
 
 function inject_fonts_header_block(theme) {
     var content = "";
     const fonts = get_fonts_if_not_fetched(),
-        css_theme = get_correct_prefix("");//theme
+        css_theme = get_correct_prefix(theme);
     if (localStorage.getItem(fonts_api_available_key) === fonts_api_available_false) {
         console.error("(inject_fonts_header_block) document.fonts not available");
         return content;
     }
     content += `    <section id="font" class="${css_theme}header_settings_font_section">\n`;
     content += `        <p>Font:</p>\n`;
-    content += `        <select id="font_options" class="${css_theme}header_settings_font_select" onchange="_get_users_choice(event)">\n`;
+    content += `        <select id="font_options" class="${css_theme}header_settings_font_select" onchange="_get_users_choice(event, '${theme}')">\n`;
     for (var i in fonts) {
         content += `            <option value="${fonts[i]}" style="font-family:'${fonts[i]}';">${fonts[i]}</option>\n`;
     }
