326279a6c2e0d29cbfdf4915110ccdb991cbde21
Hanra <37299738+HenraL@users.noreply.github.com>
2024-08-08 19:31:53 +0100

    [UPDATE] the design of the table of contents

--- frontend/assets/js/scripts/header/header.js
+++ frontend/assets/js/scripts/header/header.js
@@ -9,18 +9,20 @@ function header(ID, theme = "default") {
         logo = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAzFBMVEVHcEz////////+/v77+/vx8fL9/f309fX+/v739/f////09PXOz8/5+vr8/P3////////29vf///////84qlf8wAdGiPX8/PzsUUTqQjQsqFLrSj3S3/w6g/TqPCs0gPQgpUf85+bv9P+63sL62Nb+8ef4ycbw+PJkunkeePP81HXwgGv0jhzc5/3o9efX7N5Fr19Uj/WQy562zPr2trL94KDzoJrzoJv80Gjyl5H94qgyh9v7xzihsSp+wYV1sE5ZtXBmmvUynoWKrvzKDGT6AAAAE3RSTlMAW+TTeBLcHLMt1WsKzfUznkBIxSDAuAAAAUZJREFUKJFtktligkAMRUFZxKVuDMOAggpu1apVu+/t//9TkxBU1PsySQ4hlyGadpTd0fWOrV2R3eqyWhe80j1RpYCc7pmcI2tyaZimQw6bOTMplU9hpKIofJSUmgwtTCYq9EFhqKIJ5lbGdGIRAGhUQLNX6wRLOA2Y8vdpuvfVOJtaOjhdhL56yYrjU8cGFsRSLc4/x+DPfxBiSZN6LMlXUYXzVghBT8/7pPkdxFX28yzEO8HYI8U9dlQudMZx3AeInWWe+SrExxrhCLTre3E+M3P7FXznLn887z53a2PwGbjBLLvUP2jcYUC/FYdOA9d1g22SbN1fbizT9bUxXA+QguB4G2GlfbIFqw1i0GCzKmzDDQ1LZgPQLKHk5rAJpmSj0ykH0jxArW4V79yqF1bMkEckjYvFrTWIy0btApFsx7m68Ff1D4OdMHbngtKsAAAAAElFTkSuQmCC",
         item = document.getElementById(ID),
         css_theme = get_correct_prefix(theme),
+        toc_id = "table_of_contents_header_id",
         github_button_script = document.createElement('script');
 
     console.log(`(header) content = '${content}', home = '${home}', logo = '${logo}', item = '${JSON.stringify(item)}'`);
-    content += `    <div class="${css_theme}header_left">\n`
+    content += `    <hgroup class="${css_theme}header_left">\n`
     content += `        <a id="header_logo_link" class="${css_theme}a" href="${home}">\n`
     content += `            <img class="${css_theme}header_left_logo" src="${logo}" />\n`
     content += `        </a>\n`
-    content += `    </div>\n`
-    content += `    <div class="${css_theme}header_center">\n`
-    content += `        <h1>VerseVault - ${home}</h1>\n`
-    content += `    </div>\n`
-    content += `    <div class="${css_theme}header_right">\n`
+    content += `    </hgroup>\n`
+    content += `    <hgroup class="${css_theme}header_center">\n`
+    content += `        <h1 style="display:flex;justify-content:center;">VerseVault</h1>\n`
+    content += `        <section id="${toc_id}"></section>\n`;
+    content += `    </hgroup>\n`
+    content += `    <hgroup class="${css_theme}header_right">\n`
     content += `        <div class="${css_theme}header_right_github_buttons">\n`
     content += `            <a class="github-button" href="https://github.com/Hanra-s-work" data-color-scheme="no-preference: light; light: light; dark: dark;" data-show-count="true" aria-label="Follow @Hanra-s-work on GitHub">Follow @Hanra-s-work</a>\n`
     content += `            <a class="github-button" href="https://github.com/Hanra-s-work/verse_vault/subscription" data-icon="octicon-eye" data-show-count="true" aria-label="Watch Hanra-s-work/verse_vault on GitHub">Watch</a>\n`
@@ -43,15 +45,14 @@ function header(ID, theme = "default") {
     content += `            </section>\n`
     content += `        </aside>\n`
     content += `        <section id="${settings_id}"></section>\n`
-    content += `    </div>\n`
-    content += `<script async="" defer="" src=""></script>`
+    content += `    </hgroup>\n`
 
     console.log(`(header) content = '${content}', home = '${home}', logo = '${logo}', item = '${JSON.stringify(item)}'`);
 
     github_button_script.type = 'text/javascript';
-    github_button_script.src = 'https://buttons.github.io/buttons.js';
     github_button_script.async = true;
     github_button_script.defer = true;
+    github_button_script.src = 'https://buttons.github.io/buttons.js';
 
     item.classList.add(`${css_theme}header`);
     item.innerHTML += content;
@@ -59,4 +60,7 @@ function header(ID, theme = "default") {
 
     // adding the settings menu
     inject_settings(settings_id, theme);
+
+    // adding the table of contents
+    table_of_contents(toc_id, "", theme, true);
 }
--- frontend/assets/js/scripts/location/table_of_contents.js
+++ frontend/assets/js/scripts/location/table_of_contents.js
@@ -5,52 +5,149 @@ function add_list_current(correct_prefix = "", indentation_factor = 0, item = "S
 }
 
 function add_list_non_current(correct_prefix = "", indentation_factor = 0, link = "/", item = "Some bullet") {
-    return `<li class='${correct_prefix}table_list' style='padding-left:${indentation_factor}px'><a class='${correct_prefix}a' href='${link}'>${item}</a></li>`;
+    return `<li class='${correct_prefix}table_list' style='padding-left:${indentation_factor}px'><a class='${correct_prefix}a' href='${location.origin}${link}'>${item}</a></li>`;
 }
 
-function table_of_contents(ID, location = "", theme = "") {
-    var indentation = "indentation",
-        indentation_factor = 10,
-        theme_reference = "theme",
-        pages = [
-            { "Home": "/", indentation: 0, "theme": get_correct_prefix("default") },
-            { "Darling": "/darling", indentation: 0, "theme": get_correct_prefix("darling") },
-            { "Poems": "/poems", indentation: 0, "theme": get_correct_prefix("poems") },
-            { "Everyone": "/poems/everyone", indentation: 1, "theme": get_correct_prefix("poems") },
-            { "Henri Leblanc": "/poems/henri_leblanc", indentation: 1, "theme": get_correct_prefix("poems") },
-            { "Henry Letellier": "/poems/henry_letellier", indentation: 1, "theme": get_correct_prefix("poems") },
-            { "Thierry Montreuil": "/poems/thierry_montreuil", indentation: 1, "theme": get_correct_prefix("poems") },
-            { "Projects": "/projects", indentation: 0, "theme": get_correct_prefix("") },
-            { "Desktop Pets": "/projects/desktop_pets", indentation: 1, "theme": get_correct_prefix("") },
-            { "Planned Characters": "/projects/desktop_pets/planned_characters", indentation: 2, "theme": get_correct_prefix("") },
-            { "Quotes": "/quotes", indentation: 0, "theme": get_correct_prefix("quotes") },
-            { "Everyone": "/quotes/everyone", indentation: 1, "theme": get_correct_prefix("quotes") },
-            { "Darling": "/quotes/darling", indentation: 1, "theme": get_correct_prefix("quotes") },
-            { "About": "/about", indentation: 0, "theme": get_correct_prefix("") },
-            { "Author": "/about/author", indentation: 1, "theme": get_correct_prefix("") },
-            { "Cookies": "/about/cookies", indentation: 1, "theme": get_correct_prefix("") },
-        ],
-        correct_prefix = get_correct_prefix(theme),
-        content = document.getElementById(ID),
-        body = "";
-
-    body += `<p class='${correct_prefix}table_list_title'>Pages:</p>`;
-    body += "<nav>";
-    body += "<ul>";
+function _table_for_body(pages, css_theme, theme_key, indentation_key, location) {
+    const indentation_factor = 10;
+    var body = "";
+    body += `<p class='${css_theme}table_list_title'>Pages:</p>`;
+    body += "<nav><ul>";
     for (var i = 0; i < pages.length; i++) {
         console.log(`(table_of_contents) pages[${JSON.stringify(i)}] = ${JSON.stringify(pages[i])}`);
         for (let item in pages[i]) {
             console.log(`(table_of_contents) item = ${JSON.stringify(item)}`);
-            console.log(`(table_of_contents) pages[${i}][theme] = ${pages[i][theme_reference]} === ${correct_prefix} = ${pages[i][theme_reference] === correct_prefix}`);
-            if (location === item && pages[i][theme_reference] === correct_prefix) {
-                body += add_list_current(correct_prefix, (pages[i][indentation] * indentation_factor), item);
+            console.log(`(table_of_contents) pages[${i}][theme] = ${pages[i][theme_key]} === ${css_theme} = ${pages[i][theme_key] === css_theme}`);
+            if (location === item && pages[i][theme_key] === css_theme) {
+                body += add_list_current(css_theme, (pages[i][indentation_key] * indentation_factor), item);
             } else {
-                body += add_list_non_current(correct_prefix, (pages[i][indentation] * indentation_factor), pages[i][item], item);
+                body += add_list_non_current(css_theme, (pages[i][indentation_key] * indentation_factor), pages[i][item], item);
             }
             break;
         }
     }
-    body += "</ul>";
-    body += "</nav>";
+    body += "</ul></nav>";
+    return body;
+}
+
+/* 
+<nav class="navbar navbar-expand-md bg-body">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <div class="nav-item dropdown">
+                        <a class="dropdown-toggle active" aria-expanded="false" data-bs-toggle="dropdown" href="#">Dropdown </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">First Item</a>
+                            <a class="dropdown-item" href="#">Second Item</a>
+                            <a class="dropdown-item" href="#">Third Item</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Second Item</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+*/
+
+function _add_header_dropdown(pages, css_theme, theme_key, indentation_key, index, item) {
+    var body = "",
+        indents = index + 1;
+    const
+        nb_pages = pages.length,
+        home = location.origin,
+        prev_indentation = pages[index][indentation_key];
+    body += `<div class="nav-item dropdown">\n`;
+    body += `<a class="dropdown-toggle active" aria-expanded="false" data-bs-toggle="dropdown" href="#">${item}</a>\n`;
+    body += `<div class="dropdown-menu">\n`;
+    body += `<a class="dropdown-item" href="${home}${pages[index][item]}">${item}</a>\n`;
+    while (indents < nb_pages && pages[indents][indentation_key] > prev_indentation) {
+        for (var node in pages[indents]) {
+            if (indents + 1 < nb_pages && pages[indents + 1][indentation_key] > prev_indentation + 1) {
+                var resp = _add_header_dropdown(pages, css_theme, theme_key, indentation_key, indents, node);
+                indents = resp["i"];
+                body += resp["body"];
+                break;
+            }
+            body += `<a class="dropdown-item" href="${home}${pages[indents][node]}">${node}</a>`;
+            indents++;
+            break;
+        }
+    }
+    body += `</div></div>`;
+    return { 'i': indents, 'body': body }
+}
+
+function _table_for_header(pages, css_theme, theme_key, indentation_key) {
+    const
+        nb_pages = pages.length,
+        home = location.origin;
+    var
+        i = 0,
+        body = "";
+    body += "<nav class=\"navbar navbar-expand-md bg-body\" style=\"position:inherit;\">\n";
+    body += "<div class=\"container-fluid\">\n";
+    body += "<div class=\"collapse navbar-collapse\">";
+    body += "<ul class=\"navbar-nav\">\n";
+    while (i < nb_pages) {
+        console.log(`(table_of_contents) pages[${JSON.stringify(i)}] = ${JSON.stringify(pages[i])}`);
+        for (var item in pages[i]) {
+            if (pages[i][indentation_key] === 0 && (i + 1 < nb_pages && pages[i + 1][indentation_key] === 0)) {
+                body += `<li class="nav-item"><a class="nav-link" href="${home}${pages[i][item]}">${item}</a></li>`;
+                i++;
+            } else {
+                var response = _add_header_dropdown(pages, css_theme, theme_key, indentation_key, i, item);
+                i = response["i"];
+                body += response["body"];
+            }
+            break;
+        }
+    }
+    body += "</ul>\n";
+    body += "</div>\n";
+    body += "</div>\n";
+    body += "</nav>\n";
+    return body;
+}
+function table_of_contents(ID, location = "", theme = "", in_header = true) {
+    const
+        content = document.getElementById(ID),
+        css_theme = get_correct_prefix(theme),
+        indentation_key = "indentation",
+        theme_key = "theme",
+        empty_key = get_correct_prefix(""),
+        poems_key = get_correct_prefix("poems"),
+        darling_key = get_correct_prefix("darling"),
+        default_key = get_correct_prefix("default"),
+        quotes_key = get_correct_prefix("quotes"),
+        pages = JSON.parse(`[
+            { "Home": "/", "${indentation_key}": 0, "${theme_key}": "${default_key}" },
+            { "Darling": "/darling", "${indentation_key}": 0, "${theme_key}": "${darling_key}" },
+            { "Poems": "/poems", "${indentation_key}": 0, "${theme_key}": "${poems_key}" },
+            { "Everyone": "/poems/everyone", "${indentation_key}": 1, "${theme_key}": "${poems_key}" },
+            { "Henri Leblanc": "/poems/henri_leblanc", "${indentation_key}": 1, "${theme_key}": "${poems_key}" },
+            { "Henry Letellier": "/poems/henry_letellier", "${indentation_key}": 1, "${theme_key}": "${poems_key}" },
+            { "Thierry Montreuil": "/poems/thierry_montreuil", "${indentation_key}": 1, "${theme_key}": "${poems_key}" },
+            { "Projects": "/projects", "${indentation_key}": 0, "${theme_key}": "${empty_key}" },
+            { "Desktop Pets": "/projects/desktop_pets", "${indentation_key}": 1, "${theme_key}": "${empty_key}" },
+            { "Planned Characters": "/projects/desktop_pets/planned_characters", "${indentation_key}": 2, "${theme_key}": "${empty_key}" },
+            { "Quotes": "/quotes", "${indentation_key}": 0, "${theme_key}": "${quotes_key}" },
+            { "Everyone": "/quotes/everyone", "${indentation_key}": 1, "${theme_key}": "${quotes_key}" },
+            { "Darling": "/quotes/darling", "${indentation_key}": 1, "${theme_key}": "${quotes_key}" },
+            { "About": "/about", "${indentation_key}": 0, "${theme_key}": "${empty_key}" },
+            { "Author": "/about/author", "${indentation_key}": 1, "${theme_key}": "${empty_key}" },
+            { "Cookies": "/about/cookies", "${indentation_key}": 1, "${theme_key}": "${empty_key}" }
+        ]`);
+    var body = "";
+
+    if (in_header === true) {
+        body = _table_for_header(pages, css_theme, theme_key, indentation_key);
+    } else {
+        body = _table_for_body(pages, css_theme, theme_key, indentation_key, location);
+    }
     content.innerHTML += body;
 }
