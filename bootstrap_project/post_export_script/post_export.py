2569357c951bf10fe84823c0a026a5b1f6a2f91a
Hanra <37299738+HenraL@users.noreply.github.com>
2024-08-09 23:55:41 +0200

    [FIX] post export script

--- bootstrap_project/post_export_script/post_export.py
+++ bootstrap_project/post_export_script/post_export.py
@@ -1,18 +1,36 @@
 # This is a script that will update the json file(this acts like a config file)
+
+import os
 import json
 import datetime
+import subprocess
+from sys import argv
 
-JSON_PATH = "scripts/config.json"
+CWD = argv[1]
+
+# data = subprocess.run("tree", shell=True, capture_output=True, text=True)
+# data2 = subprocess.run(f"cd {CWD} & tree", shell=True,
+#                        capture_output=True, text=True)
+# fold_content = data.stdout
+
+# # print(f"path = {fold_content}")
+# raise RuntimeError(
+#     f"cwd = {os.getcwd()}, path = {fold_content}, argv = {
+#         argv}, argc = {len(argv)}, data2 = {data2.stdout}"
+# )
+# exit(0)
+JSON_PATH = f"{CWD}/assets/js/scripts/config.json"
 d = datetime.datetime.now()
 
 with open(JSON_PATH, "r", encoding="utf-8") as json_fp:
-    json_data = json_fp.read()
+    data = json.load(json_fp)
 
-data = json.load(json_data)
 
 data["in_devel"] = False
-data["export_date"] = f"{d.year}-{d.month}-{d.day}"
-data["export_time"] = f"{d.hour}:{d.minute}:{d.second}"
+data["export_date"] = f"{d.year}-{d.month:02d}-{d.day:02d}"
+data["export_time"] = f"{d.hour:02d}:{d.minute:02d}:{d.second:02d}"
 
 with open(JSON_PATH, "w", encoding="utf-8", newline="\n") as final_json_fp:
     json.dump(data, final_json_fp, indent=4)
+
+print("JSON file updated successfully.")
